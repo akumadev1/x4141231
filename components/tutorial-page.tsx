f5d42169c69755c69faec1adaef960e9603433e0
Julian Czapski <151388217+akumadev1@users.noreply.github.com>
2024-12-05 18:07:48 +0100

    Update tutorial-page.tsx

--- components/tutorial-page.tsx
+++ components/tutorial-page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import Image from 'next/image'
+
 import { Button } from "@/components/ui/button"
 import { useState, useRef } from 'react'
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
